31d0c8699222460bed7d19e1775dd0ab0c64dc55
sgnahore <99910473+sgnahore@users.noreply.github.com>
2024-04-03 20:17:38 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,43 @@
+import { useState } from 'react';
+import words from './wordList.json';
+import { HangmanDrawing } from './HangmanDrawing';
+import { HangmanWord } from './HangmanWord';
+import { Keyboard } from './Keyboard';
+
+function App() {
+  const [wordToGuess, setWordToGuess] = useState(() => {
+    const formula = Math.floor(Math.random() * words.length);
+
+    return words[formula];
+  });
+
+  const [guessedLetters, setGuessedLetters] = useState<string[]>([]);
+
+  const incorrectLetters = guessedLetters.filter(
+    (letter) => !wordToGuess.includes(letter)
+  );
+
+  console.log(wordToGuess);
+
+  return (
+    <div
+      style={{
+        maxWidth: '800px',
+        display: 'flex',
+        flexDirection: 'column',
+        gap: '2rem',
+        margin: '0 auto',
+        alignItems: 'center',
+      }}
+    >
+      <div style={{ fontSize: '2rem', textAlign: 'center' }}> Lose Win</div>
+      <HangmanDrawing numberOfGuesses={incorrectLetters.length} />
+      <HangmanWord />
+      <div style={{ alignSelf: 'stretch' }}>
+        <Keyboard />
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/HangmanWord.tsx
@@ -0,0 +1,32 @@
+export function HangmanWord() {
+  const word = 'test';
+  const guessedLetters = ['t', 'e', 'g'];
+
+  // to style guessed letters
+  return (
+    <div
+      style={{
+        display: 'flex',
+        gap: '.25em',
+        fontSize: '6rem',
+        fontWeight: 'bold',
+        textTransform: 'uppercase',
+        fontFamily: 'monospace',
+      }}
+    >
+      {word.split('').map((letter, index) => (
+        <span style={{ borderBottom: '.1em solid black' }} key={index}>
+          <span
+            style={{
+              visibility: guessedLetters.includes(letter)
+                ? 'visible'
+                : 'hidden',
+            }}
+          >
+            {letter}
+          </span>
+        </span>
+      ))}
+    </div>
+  );
+}
